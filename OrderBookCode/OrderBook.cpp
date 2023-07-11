57a6790d940870b2addb88e2570bb720351051df
kailashgautham <kailash.gautham@gmail.com>
2023-07-11 23:28:29 +0700

    added functions to remove bids and asks, as well as calculate spread

--- OrderBookCode/OrderBook.cpp
+++ OrderBookCode/OrderBook.cpp
@@ -13,11 +13,28 @@ void OrderBook::add_ask(int price, int amount) {
     add(price, amount, false);
 }
 
+void OrderBook::remove_bid(int price, int amount) {
+    remove(price, amount, true);
+}
+
+void OrderBook::remove_ask(int price, int amount) {
+    remove(price, amount, false);
+}
+
 void OrderBook::add(int price, int amount, bool bid) {
     if (bid) {
         bids[price] += amount;
     } else {
         asks[price] += amount;
+    }
+}
+
+void OrderBook::remove(int price, int amount, bool bid) {
+    auto& table = bid ? bids : asks;
+    auto it = table.find(price);
+    if (it != table.end()) {
+        it->second -= amount;
+        if (it->second == 0) table.erase(it);
     }
 }
 
@@ -34,5 +51,38 @@ std::ostream& operator<<(std::ostream& os, const OrderBook& book) {
         os << it->first << "\t" << it->second << std::endl;
     }
     return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const OrderBook::BidAsk& bid_ask) {
+    auto print = [&](const OrderBook::BidAsk::Entry& e, const std::string& text) {
+        bool have_value = e.is_initialized();
+        if (have_value) {
+            auto value = e.get();
+            os << value.second << text << "s @ " << value.first;
+        } else os << "NO " << text;
+    };
+    print(bid_ask.bid, "BID");
+    os << ", ";
+    print(bid_ask.ask, "ASK");
+    return os;
+}
+
+OrderBook::BidAsk OrderBook::get_bid_ask() const {
+    BidAsk result;
+    auto best_bid = bids.rbegin();
+    if (best_bid != bids.rend())
+        result.bid = *best_bid;
+    auto best_ask = asks.begin();
+    if (best_ask != asks.end())
+        result.ask = *best_ask;
+    return result;
+}
+
+boost::optional<int> OrderBook::BidAsk::spread() const {
+    boost::optional<int> result;
+    if (bid.is_initialized() && ask.is_initialized()) {
+        result = ask.get().first - bid.get().first;
+    }
+    return result;
 }
 
--- OrderBookCode/OrderBook.h
+++ OrderBookCode/OrderBook.h
@@ -5,13 +5,19 @@
 class OrderBook {
     std::map<int, int> bids, asks;
     void add(int price, int amount, bool bid);
+    void remove(int price, int amount, bool bid);
 public:
     struct BidAsk {
         typedef boost::optional<std::pair<int,int> > Entry;
+        Entry bid, ask;
+        boost::optional<int> spread() const;
     };
     bool is_empty() const;
     void add_bid(int price, int amount);
     void add_ask(int price, int amount);
+    void remove_bid(int price, int amount);
+    void remove_ask(int price, int amount);
     friend std::ostream& operator<<(std::ostream& os, const OrderBook& book);
+    friend std::ostream& operator<<(std::ostream& os, const BidAsk& bid_ask);
     BidAsk get_bid_ask() const;
 };
